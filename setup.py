09d1133df4b6ece883f03d6b4bb65c7caa81a4bc
Marc Abramowitz <marc@marc-abramowitz.com>
2014-12-18 22:05:05 -0800

    setup.py: install_requires=['pypandoc']

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Use Markdown for your project description",
     long_description=long_description,
     license='MIT',
-    setup_requires=['pypandoc'],
+    install_requires=['pypandoc'],
     py_modules=['setuptools_markdown'],
     zip_safe=False,
     entry_points="""
